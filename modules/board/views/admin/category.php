c1195b3d2c45f111583a8a3f3cdd5cc3f77b04eb
Goragod <admin@goragod.com>
2017-04-23 15:04:55 +0700

    update Webboard Category

--- modules/board/views/admin/category.php
+++ modules/board/views/admin/category.php
@@ -138,10 +138,10 @@ class View extends \Gcms\Adminview
     $item['detail'] = $this->unserialize($item['detail']);
     $item['category_id'] = '<label><input type=text class=number size=5 id=categoryid_'.$item['module_id'].'_'.$item['id'].' value="'.$item['category_id'].'" title="{LNG_Edit}"></label>';
     $icons = array(
-      empty($item['can_post']) ? '' : '<span class="icon-newtopic" title="{LNG_Posting} '.$this->cfgToStr($item['can_post']).'"></span>',
-      empty($item['can_reply']) ? '' : '<span class="icon-chat reply1" title="{LNG_Comment} '.$this->cfgToStr($item['can_reply']).'"></span>',
-      empty($item['can_view']) ? '' : '<span class="icon-visited color-red" title="{LNG_Viewing} '.$this->cfgToStr($item['can_view']).'"></span>',
-      empty($item['moderator']) ? '' : '<span class="icon-customer color-blue" title="{LNG_Moderator} '.$this->cfgToStr($item['moderator']).'"></span>'
+      empty($item['can_post']) || !is_array($item['can_post']) ? '' : '<span class="icon-newtopic" title="{LNG_Posting} '.$this->cfgToStr($item['can_post']).'"></span>',
+      empty($item['can_reply']) || !is_array($item['can_reply']) ? '' : '<span class="icon-chat reply1" title="{LNG_Comment} '.$this->cfgToStr($item['can_reply']).'"></span>',
+      empty($item['can_view']) || !is_array($item['can_view']) ? '' : '<span class="icon-visited color-red" title="{LNG_Viewing} '.$this->cfgToStr($item['can_view']).'"></span>',
+      empty($item['moderator']) || !is_array($item['moderator']) ? '' : '<span class="icon-customer color-blue" title="{LNG_Moderator} '.$this->cfgToStr($item['moderator']).'"></span>'
     );
     $item['can_reply'] = '<span class=nowrap>'.implode(' ', $icons).'</span>';
     $item['img_upload_size'] = empty($item['img_upload_type']) ? '' : '{LNG_Type} <b>'.implode(', ', $item['img_upload_type']).'</b> {LNG_File size} <b>'.$item['img_upload_size'].' Kb.</b> '.(isset($this->img_law[$item['img_law']]) ? $this->img_law[$item['img_law']] : '');
